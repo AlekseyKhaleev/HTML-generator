0935122632a473284b1cdc4a54f574b75979ca59
Aleksey Khaleev <aleksey52nn@gmail.com>
2023-11-09 13:15:41 +0300

    saving start

--- MainWidget.py
+++ MainWidget.py
@@ -1,6 +1,6 @@
 import sys  # sys нужен для передачи argv в QApplication
 
-from PyQt6 import QtWidgets
+from PyQt6 import QtWidgets, QtCore
 from collections import defaultdict as d_dict
 import html  # Это наш конвертированный файл дизайна
 from utils import generate_html
@@ -19,6 +19,7 @@ class MainApp(QtWidgets.QMainWindow, html.Ui_MainWindow):
         self.render_btn.clicked.connect(self.set_html)
         self.generate_btn.clicked.connect(self.generate)
         self.clear_btn.clicked.connect(self.text_edit.clear)
+        self.save_btn.clicked.connect(self.save_modal)
 
     def generate(self):
         self.text_edit.clear()
@@ -32,6 +33,26 @@ class MainApp(QtWidgets.QMainWindow, html.Ui_MainWindow):
     def set_html(self):
         self.data = self.text_edit.toPlainText()
         self.text_edit.setHtml(self.data)
+
+    def save_modal(self):
+        modal = QtWidgets.QMessageBox()
+        field = QtWidgets.QLineEdit(parent=modal)
+
+        field.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        accept = QtWidgets.QPushButton("Save")
+        cancel = QtWidgets.QPushButton("Cancel")
+        modal.addButton(cancel, QtWidgets.QMessageBox.ButtonRole.RejectRole)
+        modal.addButton(accept, QtWidgets.QMessageBox.ButtonRole.AcceptRole)
+        modal.setDefaultButton(accept)
+        modal.setWindowTitle("Enter filename:")
+        modal.exec()
+        if modal.clickedButton() == accept:
+            self.save_template(field.text())
+            print(field.text())
+
+    def save_template(self, filename):
+        with open(f"templates/{filename}.html", "w") as output:
+            output.write(self.data)
 
 
 def main():
