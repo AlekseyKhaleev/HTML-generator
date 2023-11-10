5a9587106778756d27820875bbb79e592533c6f4
Aleksey Khaleev <aleksey52nn@gmail.com>
2023-11-10 08:55:03 +0300

    template load done

--- MainWidget.py
+++ MainWidget.py
@@ -1,15 +1,23 @@
+from fnmatch import fnmatch
+from os import listdir
 from sys import argv
+
+from PySide6.QtCore import Signal
 from PySide6.QtWidgets import QApplication, QInputDialog, QMainWindow
+
+
 from ui_gen import Ui_MainWindow
 from utils import generate_html
 
 
 class MainApp(QMainWindow, Ui_MainWindow):
+    saved = Signal()
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.data = ""
+        self.update_templates()
 
         # ---------------------------- connections----------------------------------------
         self.text_btn.clicked.connect(self.set_text)
@@ -17,7 +25,10 @@ class MainApp(QMainWindow, Ui_MainWindow):
         self.generate_btn.clicked.connect(self.generate)
         self.clear_btn.clicked.connect(self.text_edit.clear)
         self.save_btn.clicked.connect(self.save_modal)
+        self.saved.connect(self.update_templates)
+        self.load_btn.clicked.connect(self.load_template)
         # ---------------------------------------------------------------------------------
+
 
     def generate(self):
         self.text_edit.clear()
@@ -43,6 +54,23 @@ class MainApp(QMainWindow, Ui_MainWindow):
     def save_template(self, filename):
         with open(f"templates/{filename}.html", "w") as output:
             output.write(self.data)
+        self.saved.emit()
+
+
+    def update_templates(self):
+        self.templates.clear()
+        self.templates.addItems(self.get_templates())
+
+    @staticmethod
+    def get_templates():
+        return tuple(entry for entry in listdir("templates") if fnmatch(entry, "*.html"))
+
+    def load_template(self):
+        path = str(self.templates.currentText())
+        with open(f"templates/{path}", "r") as temp:
+            self.data = temp.read()
+            self.set_text()
+
 
 
 def main():
