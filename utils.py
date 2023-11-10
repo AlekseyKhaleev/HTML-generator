1844a53d33aec3c9f951b56c81799f010f873e53
Aleksey Khaleev <aleksey52nn@gmail.com>
2023-11-10 20:28:26 +0300

    "new" arg removed, self-clean in generate_html added

--- utils.py
+++ utils.py
@@ -1,28 +1,28 @@
 class HtmlTag:
-    level = -1
-    page = "<!DOCTYPE html>\n"
+    __level = -1
+    __accumulated_tags = ""
 
-    def __init__(self, tag, *, inline=False, new=False):
+    def __init__(self, tag, *, inline=False):
         self.tag = tag
         self.inline = inline
-        if new:
-            self.__class__.page = "<!DOCTYPE html>\n"
 
     def __enter__(self):
-        self.__class__.level += 1
-        type(self).page += f"{'    ' * self.level}<{self.tag}>" + ['\n', ''][self.inline]
+        self.__class__.__level += 1
+        type(self).__accumulated_tags += f"{'    ' * self.__level}<{self.tag}>" + ['\n', ''][self.inline]
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        type(self).page += f"{['    ' * self.__class__.level, ''][self.inline]}</{self.tag}>\n"
-        self.__class__.level -= 1
+        type(self).__accumulated_tags += f"{['    ' * self.__class__.__level, ''][self.inline]}</{self.tag}>\n"
+        self.__class__.__level -= 1
 
     def fill(self, text):
-        type(self).page += ['    ' * (self.__class__.level + 1), ''][self.inline] + text + ['\n', ''][self.inline]
+        type(self).__accumulated_tags += ['    ' * (self.__class__.__level + 1), ''][self.inline] + text + ['\n', ''][
+            self.inline]
 
     @classmethod
     def generate_html(cls, sections=0, tags=0, *, inline=False):
-        with cls('html', new=True):
+        cls.__accumulated_tags += "<!DOCTYPE html>\n"
+        with cls('html'):
             with cls('head'):
                 pass
             with cls('body'):
@@ -36,8 +36,5 @@ class HtmlTag:
                                     div.fill(f"Section {sect_num + 1}, Div {tag_num + 1}")
                 with cls("footer", inline=True):
                     pass
-
-        return cls.page
-
-
-
+        result, cls.__accumulated_tags = cls.__accumulated_tags, ""
+        return result
