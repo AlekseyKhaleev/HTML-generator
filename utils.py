36f9cdb833f2af497ceddfda866e4019b0371912
Aleksey Khaleev <aleksey52nn@gmail.com>
2023-11-13 18:31:50 +0300

    new class implementation

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
-from functools import wraps
 from abc import ABC, ABCMeta, abstractmethod
+from enum import IntEnum
+from itertools import cycle, repeat
+from typing import Iterator
+
 from PySide6.QtCore import QObject
 
 
@@ -23,175 +26,67 @@ class HtmlWidget(QObject, ABC, metaclass=_ABCQObjectMeta):
         pass
 
 
-# строитель (порождающий), легковес (структурный)
-class HtmlBuilder:
-    """
-    Класс для построения HTML-тегов во вложенной структуре.
-
-    Attributes:
-        _tag (str): Имя тега HTML-элемента, который строится.
-        _inline (bool): Определяет, должен ли HTML-элемент отображаться встроенным.
-
-    Methods:
-        __init__(self, tag: str, *, inline: bool = False) -> None:
-            Инициализирует новый экземпляр класса HtmlBuilder.
-
-        __enter__(self) -> 'HtmlBuilder':
-            Входит в контекстный менеджер, открывая HTML-тег.
-
-        __exit__(self, exc_type, exc_value, traceback) -> None:
-            Выходит из контекстного менеджера, закрывая HTML-тег.
-
-        fill(self, text: str) -> None:
-            Заполняет HTML-элемент данным текстом.
-
-        @classmethod
-        def generate_html(cls, tag: str, content: str, *, inline: bool = False) -> str:
-            Генерирует полный HTML-элемент с заданным количеством секций и тегов,
-            а также с дополнительным атрибутом "inline".
-
-    Пример использования:
-        1. with HtmlBuilder('div') as builder:
-               builder.fill('Привет, мир!')
-
-        2. html = HtmlBuilder.generate_html('div', 'Hello, world!', inline=True)
-           print(html)
-    """
-    _level = -1
-    _accumulated_tags = ""
-    __instances = dict()
-
-    # легковес (структурный)
-    def __new__(cls, *args, **kwargs):
-        return cls.__instances.setdefault(hash(args + tuple(kwargs.items())), super().__new__(cls))
-
-    def __init__(self, tag: str, *, inline: bool = False) -> None:
-        """
-        Инициализирует новый экземпляр класса HtmlBuilder.
-
-        Args:
-            tag (str): Имя тега HTML-элемента, который строится.
-            inline (bool, optional): Определяет, должен ли HTML-элемент отображаться встроенным.
-                По умолчанию False.
-        """
-        self._tag = tag
-        self._inline = inline
-
-    def __enter__(self):
-        """
-        Входит в контекстный менеджер, открывая HTML-тег.
-
-        Returns:
-            HtmlBuilder: Экземпляр класса HtmlBuilder.
-        """
-        type(self)._level += 1
-        type(self)._accumulated_tags += f"{'    ' * type(self)._level}<{self._tag}>" + ['\n', ''][self._inline]
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        """
-        Выходит из контекстного менеджера, закрывая HTML-тег.
-
-        Args:
-            exc_type: Тип исключения.
-            exc_value: Значение исключения.
-            traceback: Отслеживание исключения.
-        """
-        type(self)._accumulated_tags += f"{['    ' * type(self)._level, ''][self._inline]}</{self._tag}>\n"
-        type(self)._level -= 1
-
-    def fill(self, text):
-        """
-        Заполняет HTML-элемент данным текстом.
-
-        Args:
-            text (str): Текст для заполнения HTML-элемента.
-        """
-        type(self)._accumulated_tags += ['    ' * (type(self)._level + 1), ''][self._inline] + text + ['\n', ''][
-            self._inline]
-
-    @classmethod
-    def generate_html(cls, sections=0, tags=0, *, inline=False):
-        """
-        Генерирует полный HTML-элемент с заданным количеством секций и тегов,
-        а также с дополнительным атрибутом "inline".
-
-        Args:
-            sections (int, optional): Количество секций HTML-элемента.
-                По умолчанию 0.
-            tags (int, optional): Количество тегов HTML-элемента.
-                По умолчанию 0.
-            inline (bool, optional): Определяет, должен ли HTML-элемент отображаться встроенным.
-                По умолчанию False.
-
-        Returns:
-            str: Сгенерированный HTML-код элемента.
-        """
-
-        cls._accumulated_tags = "<!DOCTYPE html>\n"
-        with cls('html'):
-            with cls('head'):
-                pass
-            with cls('body'):
-                with cls('header', inline=True):
-                    pass
-                with cls('main'):
-                    for sect_num in range(sections):
-                        with cls('section'):
-                            for tag_num in range(tags):
-                                with cls('div', inline=inline) as div:
-                                    div.fill(f"Section {sect_num + 1}, Div {tag_num + 1}")
-                with cls("footer", inline=True):
-                    pass
-        result, cls._accumulated_tags = cls._accumulated_tags, ""
-        return result
-
-
-# декоратор (структурный)
-def border_divs_enter(depth=2, color='black'):
-    def decorator(func):
-        @wraps(func)
-        def enter_wrapper(self):
-            match self._tag:
-                case 'head':
-                    res = func(self)
-                    with self.__class__('style') as style:
-                        style.fill(f".bordered {{ border: {depth}px solid {color}; }}")
-                    return res
-                case 'div':
-                    self._tag = 'div class="bordered"'
-            return func(self)
-
-        return enter_wrapper
-
-    return decorator
-
-
-def border_divs_exit(func):
-    @wraps(func)
-    def exit_wrapper(self, *args, **kwargs):
-        if self._tag == 'div class="bordered"':
-            self._tag = 'div'
-        return func(self, *args, **kwargs)
-
-    return exit_wrapper
-
-
-class HtmlBorderedBuilder(HtmlBuilder):
-    @border_divs_enter()
-    def __enter__(self):
-        return super().__enter__()
-
-    @border_divs_exit
-    def __exit__(self, exc_type, exc_value, traceback):
-        return super().__exit__(exc_type, exc_value, traceback)
-
-
 # стратегия (поведенческий)
 class HtmlBuildStrategy:
     def __init__(self, *, bordered):
-        self.builder = HtmlBorderedBuilder if bordered else HtmlBuilder
+        # self.builder =
+        pass
 
     # адаптер (структурный)
     def build_page(self, obj: HtmlWidget):
         return self.builder.generate_html(obj.sections, obj.divs, inline=obj.inline)
+
+
+class HtmlTag(ABC):
+    __instances = dict()
+
+    def __new__(cls, tag_name, *args, **kwargs):
+        return cls.__instances.setdefault(tag_name, super().__new__(cls))
+
+    def __init__(self, tag_name: str):
+        self.tag_iter = self._get_tag_iter(tag_name)
+        self._states = cycle(IntEnum("State", "CLOSED OPEN"))
+        self.state = self._states.__next__()
+
+    @property
+    @abstractmethod
+    def tag(self) -> str: ...
+
+    @abstractmethod
+    def _get_tag_iter(self, tag_name: str) -> Iterator[str]:
+        pass
+
+
+class SimpleTag(HtmlTag):
+    @property
+    def tag(self) -> str:
+        return next(self.tag_iter)
+
+    def _get_tag_iter(self, tag_name: str) -> Iterator:
+        return repeat(f"<{tag_name}>")
+
+
+class DoubleTag(HtmlTag):
+    @property
+    def tag(self) -> str:
+        self.state = next(self._states)
+        return next(self.tag_iter)
+
+    def _get_tag_iter(self, tag_name) -> Iterator:
+        return cycle((f"<{tag_name}>", f"</{tag_name}>"))
+
+
+class PageBuilder:
+    def build_tag(self ):
+        pass
+
+
+# def build_base(self):
+
+div = DoubleTag("div")
+img = SimpleTag("img")
+
+for _ in range(4):
+    print(div.tag)
+    print(img.tag)
+    print(div.tag)
