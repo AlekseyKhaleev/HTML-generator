4b23341d94290cae549a336fadc49fdf6d7a4f48
Aleksey Khaleev <aleksey52nn@gmail.com>
2023-11-16 12:02:25 +0300

    ...

---
+++ html_base/html_tags.py
@@ -0,0 +1,55 @@
+from abc import ABC, abstractmethod
+from itertools import cycle, repeat
+from typing import Iterator
+
+
+class HtmlTag(ABC):
+    def __init__(self, tag_name: str):
+        self.__tag_name = tag_name
+        self.tag = tag_name
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self.__tag_name})"
+
+    def __hash__(self):
+        return hash(self.__tag_name)
+
+    def __eq__(self, other):
+        if not isinstance(other, HtmlTag):
+            return NotImplemented
+        return self.__tag_name == other.__tag_name
+
+    @abstractmethod
+    def _get_tag_gen(self, tag_name: str) -> Iterator: ...
+
+    @property
+    def tag(self) -> str:
+        return next(self._tag_iter)
+
+    @tag.setter
+    def tag(self, tag_name: str):
+        self._tag_iter = self._get_tag_gen(tag_name)
+
+
+class SingleTag(HtmlTag):
+    def _get_tag_gen(self, tag_name: str) -> Iterator:
+        return repeat(f"<{tag_name}>")
+
+
+class DoubleTag(HtmlTag):
+    def _get_tag_gen(self, tag_name: str) -> Iterator:
+        return cycle((f"<{tag_name}>", f"</{tag_name}>"))
+
+
+class Singleton:
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+
+
+
+
--- utils.py
+++ utils.py
@@ -1,12 +1,11 @@
 from abc import ABC, ABCMeta, abstractmethod
-from enum import IntEnum
-from itertools import cycle, repeat, zip_longest, chain
-from typing import Iterator, Self
+from dataclasses import dataclass, field
 from collections import deque
 
 from PySide6.QtCore import QObject
-from constants import STATE, MAX_CALLS, TAB
-from html_tags import HtmlTag, DoubleTag, SingleTag, Singleton, create_tag_type
+
+from html_base.html_tags import HtmlTag, DoubleTag, SingleTag, Singleton
+from html_base.constants import HTML_SINGLES, HTML_DOUBLES, HTML_SINGLETONS
 
 
 class _ABCQObjectMeta(type(QObject), ABCMeta): ...
@@ -30,88 +29,28 @@ class HtmlWidget(QObject, ABC, metaclass=_ABCQObjectMeta):
 
 
 # стратегия (поведенческий)
-class HtmlBuildStrategy:
-    def __init__(self, *, bordered):
-        # self.builder =
-        pass
-
-    # адаптер (структурный)
-    # def build_page(self, obj: HtmlWidget):
-    #     return self.builder.generate_html(obj.sections, obj.divs, inline=obj.inline)
-
-
-class TreeBuilder(ABC):
-    def __init__(self, *, tree: dict | None = None):
-        self.tree = tree if tree is not None else dict()
-        self.level = 0
-        self.call_stack = deque()
-        self.res = str()
-
-    @abstractmethod
-    def open_tag(self, tag: HtmlTag, *, inplace: bool = True): ...
-
-    @abstractmethod
-    def close_tag(self): ...
-
-    @abstractmethod
-    def add_value(self, value: str): ...
+# class HtmlBuildStrategy:
+#     def __init__(self, *, bordered):
+#         # self.builder =
+#         pass
+#
+#     # адаптер (структурный)
+#     def build_page(self, obj: HtmlWidget):
+#         return self.builder.generate_html(obj.sections, obj.divs, inline=obj.inline)
 
 
-class TagBuilder(TreeBuilder):
+class Builder(ABC, Singleton):
+    def add(self, value: str):
+        content = self.create_content(value)
 
-    def add_inplace(self, tag: HtmlTag, key: DoubleTag) -> None:
-        self.tree[key].append(tag)
-
-    def add_nested(self, tag: HtmlTag, key: DoubleTag) -> None:
-        self.tree[key].append({tag: []})
-
-    def open_tag(self, tag: HtmlTag, *, inplace: bool = True):
-        self.res += "\t" * self.level + tag.tag + "\n"
-        self.call_stack.append(tag)
-        self.level += 1
-
-    def close_tag(self):
-        self.level = 0 if self.level - 1 < 0 else self.level - 1
-        last_tag = self.call_stack.pop()
-        if isinstance(last_tag, DoubleTag):
-            self.res += "\t" * self.level + last_tag.tag + "\n"
-
-    def add_value(self, value: str):
-        self.res += "\t" * self.level + value + "\n"
-
-    def close_all(self):
-        while self.call_stack:
-            self.close_tag()
-
-
-class HtmlDirector:
-    def __init__(self):
-        self.base_tree = {
-            SingleTag("!DOCTYPE html"): {},
-            DoubleTag("html"): {
-                DoubleTag("head"): {},
-                DoubleTag("body"): {
-                    DoubleTag("header"): {},
-                    DoubleTag("main"): [],
-                    DoubleTag("footer"): {}
-                }
-            }
-        }
-        self.main_builder = TagBuilder(tree=self.base_tree[DoubleTag("html")][DoubleTag("body")])
-
-    def build_tree(self, *, sections=1, divs=1):
-        for _ in range(sections):
-            self.main_builder.add_nested(DoubleTag("section"), DoubleTag("main"))
-        for section in self.main_builder.tree[DoubleTag("main")]:
-            for _ in range(divs):
-                section[DoubleTag("section")].append(DoubleTag("div"))
-
-    def result(self):
-        return self.base_tree
-
-div = create_tag_type("div")
-
-tag = div()
-
-
-print(div.tag)
+    @staticmethod
+    # фабричный метод | стратегия
+    def create_content(value: str) -> HtmlTag | str:
+        if value in HTML_SINGLES:
+            return SingleTag(value)
+        elif value in HTML_DOUBLES:
+            return DoubleTag(value)
+        elif value in HTML_SINGLETONS:
+            return type("HtmlSingleton", (DoubleTag, Singleton), {})(value)
+        else:
+            return value
